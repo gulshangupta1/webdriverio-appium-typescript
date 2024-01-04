137a91a278ee573b807eac36271623b0269107d7
gulshangupta003 <gulshangupta003@gmail.com>
2024-01-04 18:47:02 +0530

    implemented loggers

--- test/specs/logout.test.ts
+++ test/specs/logout.test.ts
@@ -1,15 +1,18 @@
+import { FileUtils } from "../../utilities/fileUtils";
+import { LoginDetails } from "../resources/customTypes/loginDetails";
 import { HomeScreen } from "../screens/homeScreen";
 import { LoginScreen } from "../screens/loginScreen";
+import * as loginDetailsJson from "./../resources/testdata/loginDetails.json";
 
 describe('Logout test scenarios', () => {
     it('Logout test', async () => {
-        const username: string = 'bob@example.com';
-        const password: string = '10203040';
+        const loginDetails: LoginDetails = FileUtils.convertJsonToCustomType(loginDetailsJson);
+
         const homeScreen: HomeScreen = new HomeScreen();
         const loginScreen: LoginScreen = new LoginScreen();
 
         // Login
-        await homeScreen.login(username, password);
+        await homeScreen.login(loginDetails.username, loginDetails.password);
         // Logout
         await homeScreen.logout();
         await (await loginScreen.getLoginButtonEle()).waitForDisplayed();
---
+++ utilities/reporting/loggerHelper.ts
@@ -0,0 +1,62 @@
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+import path from 'path';
+import winston, { Logger } from 'winston';
+
+export let LOGGER: Logger;
+
+export namespace LoggerHelper {
+    export function setupLogger(specName?: string) {
+        const reportFolderPath: string = `${process.cwd()}/logs`;
+        let loggerFileName: string = 'log.log';
+
+        try {
+            if (!existsSync(reportFolderPath)) {
+                mkdirSync(reportFolderPath);
+            }
+
+            if (!(specName === undefined || specName === '' || specName === null)) {
+                loggerFileName = `${specName.replace(/[^\w\d]/gi, '_')}.log`;
+            }
+
+            // declare format of the logger
+            const myFormat: winston.Logform.Format = winston.format.combine(
+                winston.format.colorize(),
+                winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
+                winston.format.align(),
+                winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`),
+            );
+
+            const logFilePath = path.join(reportFolderPath, loggerFileName);
+            if (!existsSync(logFilePath)) {
+                writeFileSync(path.join(reportFolderPath, loggerFileName), '');
+            }
+
+            // initialize the logger
+            // if (LOGGER == undefined || LOGGER == null) {
+            LOGGER = winston.createLogger({
+                level: 'debug',
+                format: myFormat,
+                transports: [
+                    new winston.transports.File({
+                        filename: path.join(reportFolderPath, loggerFileName),
+                        level: 'info',
+                        maxsize: 5120000,
+                    }),
+                    new winston.transports.Console({
+                        level: 'debug',
+                    }),
+                ],
+            });
+            LOGGER.info('-----------------------------');
+            LOGGER.info('LOGGER setup complete');
+            LOGGER.info('-----------------------------');
+            LOGGER.info(`generating log file at ${reportFolderPath}/${loggerFileName}`);
+            // }
+        } catch (error) {
+            console.info('-----------------------------');
+            console.info('LOGGER setup Failed');
+            console.info('-----------------------------');
+            throw new Error(error);
+        }
+    }
+}
