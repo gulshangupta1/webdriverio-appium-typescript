8c046aecd3dc11e0cc4d56bc5d348d04b6bd6ed4
gulshangupta003 <gulshangupta003@gmail.com>
2024-01-04 12:48:24 +0530

    implemented data hiding from test file

---
+++ utilities/fileUtils.ts
@@ -0,0 +1,11 @@
+export class FileUtils {
+
+    /**
+     * Convert a JSON object to a custom type.
+     * @param json - The JSON object.
+     * @returns - The custom type object.
+     */
+    static convertJsonToCustomType<T>(json: Record<string, unknown>): T {
+        return json as T;
+    }
+}
